c237672561a2f419b766bf123332c6654b065fc5
LaysLaChips <cmancian3@gmail.com>
2025-07-02 12:14:30 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void sayHello_shouldReturnHelloMessage() {
+        HelloService helloService = new HelloService();
+        String result = helloService.sayHello();
+        assertEquals("Hello, CI/CD!", result);
+    }
 }
