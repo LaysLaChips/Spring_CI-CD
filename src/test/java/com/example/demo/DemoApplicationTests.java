d4ea44065b86030542a76cd6c6877c54a7158263
LaysLaChips <cmancian3@gmail.com>
2025-07-02 12:17:27 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DemoApplicationTests {
 
     @Test
-    void sayHello_shouldReturnHelloMessage() {
+    void testSayHello() {
         HelloService helloService = new HelloService();
         String result = helloService.sayHello();
         assertEquals("Hello, CI/CD!", result);
